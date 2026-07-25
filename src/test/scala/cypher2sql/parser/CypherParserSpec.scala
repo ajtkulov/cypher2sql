f5431e82d964cfaba66275fee632676d789814b5
Pavel Ajtkulov <ajtkulov@gmail.com>
2026-07-25 00:31:29 -0700

    -

--- src/test/scala/cypher2sql/parser/CypherParserSpec.scala
+++ src/test/scala/cypher2sql/parser/CypherParserSpec.scala
@@ -98,6 +98,74 @@ class CypherParserSpec extends AnyFlatSpec with Matchers:
 
     q.clauses(1).asInstanceOf[Return].items shouldBe List(ReturnItem(Star, None))
 
+  it should "parse multi-MATCH person/SNILS query" in:
+    val q = parseOk(
+      """
+        MATCH (p:Person)
+        WHERE p.last_name = 'John' AND p.first_name = 'Smith'
+        MATCH (p)-[se:HAS_SNILS]->(s:SNILS)
+        MATCH (p1:Person)-[se1:HAS_SNILS]->(s)
+        WHERE p1 <> p
+          AND p.last_name <> p1.last_name
+        RETURN p, s, se, p1
+        LIMIT 100
+      """
+    )
+    q.clauses should have size 4
+
+    val m1 = q.clauses(0).asInstanceOf[Match]
+    m1.pattern.parts.head.element shouldBe
+      NodePattern(Some("p"), List("Person"), None)
+    m1.where shouldBe Some(
+      And(
+        Comparison(Property(Variable("p"), "last_name"), CmpOp.Eq, StringLit("John")),
+        Comparison(Property(Variable("p"), "first_name"), CmpOp.Eq, StringLit("Smith"))
+      )
+    )
+
+    val m2 = q.clauses(1).asInstanceOf[Match]
+    m2.where shouldBe None
+    m2.pattern.parts.head.element match
+      case PathPattern(start, List((rel, end))) =>
+        start shouldBe NodePattern(Some("p"), Nil, None)
+        rel shouldBe RelationshipPattern(
+          Some("se"),
+          List("HAS_SNILS"),
+          None,
+          None,
+          Direction.Outgoing
+        )
+        end shouldBe NodePattern(Some("s"), List("SNILS"), None)
+      case other => fail(s"expected SNILS path, got $other")
+
+    val m3 = q.clauses(2).asInstanceOf[Match]
+    m3.pattern.parts.head.element match
+      case PathPattern(start, List((rel, end))) =>
+        start shouldBe NodePattern(Some("p1"), List("Person"), None)
+        rel.variable shouldBe Some("se1")
+        rel.types shouldBe List("HAS_SNILS")
+        end shouldBe NodePattern(Some("s"), Nil, None)
+      case other => fail(s"expected second person path, got $other")
+    m3.where shouldBe Some(
+      And(
+        Comparison(Variable("p1"), CmpOp.Neq, Variable("p")),
+        Comparison(
+          Property(Variable("p"), "last_name"),
+          CmpOp.Neq,
+          Property(Variable("p1"), "last_name")
+        )
+      )
+    )
+
+    val ret = q.clauses(3).asInstanceOf[Return]
+    ret.items shouldBe List(
+      ReturnItem(Variable("p"), None),
+      ReturnItem(Variable("s"), None),
+      ReturnItem(Variable("se"), None),
+      ReturnItem(Variable("p1"), None)
+    )
+    ret.limit shouldBe Some(IntegerLit(100))
+
   it should "return a clear error on invalid input" in:
     CypherParser.parse("MATCH (n RETURN n") match
       case Left(err) => err should include("Parse error")
